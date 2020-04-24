65eb0395b35d39e635d6e2f6d6be405a02d8bb10
Tristan Guichaoua <baanlohdev@gmail.com>
2020-04-24 17:02:16 +0200

    add Command settings to CommandDef

--- src/models/CommandDef.ts
+++ src/models/CommandDef.ts
@@ -1,7 +1,15 @@
 import { SignatureDef } from "./SignatureDef";
+import { CommandSet } from "..";
 
 export interface CommandDef {
     description: string;
     signatures?: SignatureDef[];
     subs?: { [name: string]: CommandDef };
+
+    onInit: (context: any, commandSet: CommandSet) => void | Promise<void>;
+
+    inherit?: boolean;
+    ignore?: boolean;
+    keepCommandMessage?: boolean;
+    dev?: boolean;
 }
