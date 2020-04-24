196003e0e9ee323d41cd5547b4c937a9d78ab0ce
Tristan Guichaoua <baanlohdev@gmail.com>
2020-04-24 16:52:37 +0200

    impl SignatureDef

---
+++ src/models/SignatureDef.ts
@@ -0,0 +1,9 @@
+import { CommmandQuery } from "./CommandQuery";
+import { ArgDef } from "./ArgDef";
+import { FlagDef } from "./FlagDef";
+
+export interface SignatureDef {
+    executor: (query: CommmandQuery) => any;
+    args?: { [name: string]: ArgDef };
+    flags?: { [name: string]: FlagDef }
+}
