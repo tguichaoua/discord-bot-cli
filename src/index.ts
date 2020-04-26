e2782416f2fff610dd6b53a8e259c1124b4739ad
Tristan Guichaoua <baanlohdev@gmail.com>
2020-04-26 17:30:45 +0200

    export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { Command } from "./models/Command";
 export { CommandSet } from "./models/CommandSet";
+export { ParseOptions } from "./models/ParseOptions";
+export { CommandResult } from "./models/CommandResult";
+export { Localization } from "./models/localization/Localization";
 
 export { CommandQuery } from "./models/query/CommandQuery";
 export { HelpQuery } from "./models/query/HelpQuery";
