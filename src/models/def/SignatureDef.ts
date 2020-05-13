a4ad64a89a12a31b3c8bb8d4a9dc3ba88d707fcf
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-13 18:58:12 +0200

    add generic type on command related defs

--- src/models/def/SignatureDef.ts
+++ src/models/def/SignatureDef.ts
@@ -2,9 +2,9 @@ import { CommandQuery } from "../query/CommandQuery";
 import { ArgDef } from "./ArgDef";
 import { FlagDef } from "./FlagDef";
 
-export interface SignatureDef {
+export interface SignatureDef<Context = any> {
     /** Called when this signature is executed. */
-    executor: (query: CommandQuery) => any | Promise<any>;
+    executor: (query: CommandQuery<Context>) => any | Promise<any>;
     /** Defined what are arguments of this signature and how they must be parsed. */
     args?: { [name: string]: ArgDef };
     /** Defined what are flags of this signature and how they must be parsed. */
