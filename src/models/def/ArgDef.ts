146a3c32fe0d30b3b4bc488c2328ed60096d8d15
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-05-13 18:08:58 +0200

    Fix doc v3.x (#17)
    * CommandDef doc
    
    * SignatureDef doc
    
    * ParsableDef, ArgDef, FlagDef doc
    
    * update docs

--- src/models/def/ArgDef.ts
+++ src/models/def/ArgDef.ts
@@ -1,2 +1,5 @@
 import { ParsableDef } from "./ParsableDef";
-export type ArgDef = ParsableDef & { optional?: boolean };
+export type ArgDef = ParsableDef & {
+    /** If set to true, this argument can be omitted. In this case defaultValue is used. (default is false) */
+    optional?: boolean
+};
--- src/models/def/SignatureDef.ts
+++ src/models/def/SignatureDef.ts
@@ -3,8 +3,12 @@ import { ArgDef } from "./ArgDef";
 import { FlagDef } from "./FlagDef";
 
 export interface SignatureDef {
+    /** Called when this signature is executed. */
     executor: (query: CommandQuery) => any | Promise<any>;
+    /** Defined what are arguments of this signature and how they must be parsed. */
     args?: { [name: string]: ArgDef };
+    /** Defined what are flags of this signature and how they must be parsed. */
     flags?: { [name: string]: FlagDef };
+    /** Define a name and a description for a rest argument. Used for help purpose. */
     rest?: { name: string, description?: string };
 }
