139aa02850715644b1ac808488135b066682b1b5
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-13 15:01:27 +0200

    ParsableDef, ArgDef, FlagDef doc

--- src/models/def/FlagDef.ts
+++ src/models/def/FlagDef.ts
@@ -1,4 +1,7 @@
 import { Char } from "../../utils/char"
-import {  ParsableDef } from "./ParsableDef";
+import { ParsableDef } from "./ParsableDef";
 
-export type FlagDef = ParsableDef & { shortcut?: Char };
+export type FlagDef = ParsableDef & {
+    /** Shortcut version of the flag. */
+    shortcut?: Char
+};
