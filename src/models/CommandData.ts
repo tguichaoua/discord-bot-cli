34dc158c388c4fc097f379a9d13b64fe7d3a975d
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-23 20:10:03 +0200

    🏷️ CommandData

---
+++ src/models/CommandData.ts
@@ -0,0 +1,11 @@
+import { CommandDefinition } from "./definition/CommandDefinition";
+import { CommandExecutor } from "./CommandExecutor";
+
+
+export interface CommandData<T extends CommandDefinition> {
+    /** Called when the command is executed. */
+    executor?: CommandExecutor<T>;
+
+    /** Sub-commands data. */
+    subs: { [name in keyof T["subs"]] : CommandData<T["subs"][name]> }
+}
