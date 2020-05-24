932c36abb0f3ba5efd55ba7982ef263d990d0db3
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-24 10:08:52 +0200

    ✨ arg are undefined if optional & no default value

--- src/models/CommandExecutor.ts
+++ src/models/CommandExecutor.ts
@@ -7,7 +7,13 @@ import { ParseOptions } from "./ParseOptions";
 
 export type CommandExecutor<T extends CommandDefinition> =
     (
-        args: { [name in keyof T["args"]]: ParsableTypeOf<NonNullable<T["args"]>[name]["type"]> },
+        args: { [name in keyof T["args"]]: (
+            ParsableTypeOf<NonNullable<T["args"]>[name]["type"]>
+            | (
+                NonNullable<T["args"]>[name]["optional"] extends true ? undefined extends NonNullable<T["args"]>[name]["defaultValue"] ?
+                undefined : never : never
+            )
+        ) },
         flags: { [name in keyof T["flags"]]: ParsableTypeOf<NonNullable<T["args"]>[name]["type"]> },
         others: {
             rest: string[],
