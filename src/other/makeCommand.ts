e44e742fdd4599fa140d3a8b101ed4aac7261e48
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-24 10:48:17 +0200

    ✨ makeCommand

--- src/other/makeCommand.ts
+++ src/other/makeCommand.ts
@@ -2,6 +2,13 @@ import { CommandDefinition } from "../models/definition/CommandDefinition";
 import { CommandData } from "../models/CommandData";
 
 export function makeCommand<T extends CommandDefinition>(name: string, definition: T): CommandData<T> {
-    // TODO
-    return {} as any;
+    let subs = {} as any;
+    for (let key in definition.subs) {
+        subs[key] = makeCommand(key, definition.subs[key]);
+    }
+
+    return {
+        data: definition,
+        subs
+    };
 }
