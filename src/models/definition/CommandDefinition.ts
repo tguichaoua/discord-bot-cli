c5a36bd39754ea0a52869a2392df96165a18aad2
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-24 10:32:33 +0200

    🎨 use readonly in CommandDefinition

--- src/models/definition/CommandDefinition.ts
+++ src/models/definition/CommandDefinition.ts
@@ -3,25 +3,25 @@ import { FlagDefinition } from "./FlagDefinition";
 
 export interface CommandDefinition {
     /** The description of this command. Used by help command. */
-    description?: string;
+    readonly description?: string;
     /** Arguments that must be passed to the command. */
-    args?: { [name: string]: ArgDefinition };
+    readonly args?: { readonly [name: string]: ArgDefinition };
     /** Flags that can be used with this command. */
-    flags?: { [name: string]: FlagDefinition };
+    readonly flags?: { readonly [name: string]: FlagDefinition };
     /** Define a name and a description for a rest argument. Used for help purpose. */
-    rest?: { name: string, description?: string };
+    readonly rest?: { name: string, description?: string };
     
     /** Sub-commands of this command. */
-    subs?: { [name: string]: CommandDefinition };
+    readonly subs?: { readonly [name: string]: CommandDefinition };
 
     /** If set to true, undefined inheritable properties are inherited from parent command. (default is false). */
-    inherit?: boolean;
+    readonly inherit?: boolean;
     /** If set to true, this command is not loaded. (default is false). [inheritable] */
-    ignore?: boolean;
+    readonly ignore?: boolean;
     /** If set to true, the message that trigger this command is automatically deleted. (default is true). [inheritable] */
-    deleteCommandMessage?: boolean;
+    readonly deleteCommandMessage?: boolean;
     /** If set to true, only user registred as dev via `ParseCommand.devIDs` can execute, get help or list this command. (default is false). [inheritable] */
-    dev?: boolean;
+    readonly dev?: boolean;
     /** If set to true, this command can only be executed from a server. (default is false). [inheritable] */
-    guildOnly?: boolean;
+    readonly guildOnly?: boolean;
 }
