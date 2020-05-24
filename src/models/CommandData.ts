e7353cd18fbc7f1ec1a799c7e5f7327dd6549818
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-24 10:39:02 +0200

    🎨 readonly on CommandData

--- src/models/CommandData.ts
+++ src/models/CommandData.ts
@@ -1,14 +1,13 @@
 import { CommandDefinition } from "./definition/CommandDefinition";
 import { CommandExecutor } from "./CommandExecutor";
 
-
 export interface CommandData<T extends CommandDefinition> {
     /** @internal */
-    data: T;
+    readonly data: T;
 
     /** Called when the command is executed. */
     executor?: CommandExecutor<T>;
 
     /** Sub-commands data. */
-    subs: { [name in keyof T["subs"]] : CommandData<T["subs"][name]> }
+    readonly subs: { readonly [name in keyof T["subs"]]: CommandData<T["subs"][name]> }
 }
