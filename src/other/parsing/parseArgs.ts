bcf27c21a96a13c55325780910a9a466fe320e7c
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-31 15:21:39 +0200

    🐛 fix parseArgs

--- src/other/parsing/parseArgs.ts
+++ src/other/parsing/parseArgs.ts
@@ -1,5 +1,5 @@
 import { Message } from "discord.js";
-import { ParsableType, getDefaultValue } from "../../models/ParsableType";
+import { ParsableType } from "../../models/ParsableType";
 import { parseValue } from "./parseValue";
 import { ArgDefinition } from "../../models/definition/ArgDefinition";
 
@@ -8,13 +8,13 @@ export function parseArgs(
     argDefinitions: ReadonlyMap<string, ArgDefinition>
 ) {
     const args = [...inputArguments];
-    const argValues = new Map<string, ParsableType>();
+    const argValues = new Map<string, ParsableType | undefined>();
 
     for (const [name, def] of argDefinitions) {
-        let value: ParsableType;
+        let value: ParsableType | undefined;
         if (args.length === 0) {
             if (!def.optional) return;
-            value = def.defaultValue ?? getDefaultValue(def.type);
+            value = def.defaultValue;
         } else {
             const parsed = parseValue(def, message, args.shift() as string);
             if (!parsed) return;
