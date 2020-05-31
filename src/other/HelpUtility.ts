09f570a2a4c7ca99ac00a132e1f704e6d27d440d
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-31 15:34:27 +0200

    ⚡ update HelpUtility module

--- src/other/HelpUtility.ts
+++ src/other/HelpUtility.ts
@@ -1,11 +1,11 @@
 import { Command } from "../models/Command";
 import { Localization } from "../models/localization/Localization";
 import { MessageEmbed } from "discord.js";
-import { Arg } from "../models/parsable/Arg";
 import { ParsableLocalization } from "../models/localization/ParsableLocalization";
 import { TypeNameLocalization } from "../models/localization/TypeNameLocalization";
 import { Signature } from "../models/Signature";
-import { Flag } from "../models/parsable/Flag";
+import { FlagDefinition } from "../models/definition/FlagDefinition";
+import { ArgDefinition } from "../models/definition/ArgDefinition";
 
 export namespace HelpUtility {
 
@@ -23,22 +23,27 @@ export namespace HelpUtility {
                 .setTitle(prefix + name)
                 .setDescription((command.guildOnly ? `[${localization.help.guildOnlyTag}]\n` : "") + description);
 
+            const usageString = Array.from(command.args.entries())
+                .map(([name, arg]) => Arg.usageString(name, arg, loc?.args ? loc.args[name] : undefined))
+                .join(" ")
+                + (command.rest ? `[...${loc?.rest?.name ?? command.rest.name}]` : "");
+
             // if there is only 1 signature without any argument (nor rest), don't display this signature.
-            if (!(command.signatures.length === 1 && command.signatures[0].arguments.length === 0 && !command.signatures[0].rest)) {
-                for (const s of command.signatures) {
-                    const description =
-                        Signature.argumentsDescription(s, localization) +
-                        "\n" +
-                        Signature.flagsDescription(s, localization);
-                    embed.addField(
-                        prefix + name + ' ' + Signature.usageString(s, localization),
-                        description.length === 0 ? "*" : description
-                    );
-                }
-            }
+            // if (!(command.signatures.length === 1 && command.signatures[0].arguments.length === 0 && !command.signatures[0].rest)) {
+            //     for (const s of command.signatures) {
+            //         const description =
+            //             Signature.argumentsDescription(s, localization) +
+            //             "\n" +
+            //             Signature.flagsDescription(s, localization);
+            //         embed.addField(
+            //             prefix + name + ' ' + Signature.usageString(s, localization),
+            //             description.length === 0 ? "*" : description
+            //         );
+            //     }
+            // }
 
             let str = '';
-            for (const cmd of command.getSubs())
+            for (const cmd of command.subs.values())
                 str += `**${cmd.name}** ${localization.commands[cmd.name]?.description ?? cmd.description}\n`;
             if (str !== '')
                 embed.addField('Sub Commands', str);
@@ -47,51 +52,51 @@ export namespace HelpUtility {
         }
     }
 
-    export namespace Signature {
-        export function usageString(signature: Signature, localization?: Localization) {
-            const loc = localization?.commands[signature.command.name];
-            signature.arguments
-                .map(a => Arg.usageString(a, loc?.args ? loc.args[a.name] : undefined))
-                .join(" ")
-                + (signature.rest ? `[...${loc?.rest?.name ?? signature.rest.name}]` : "");
-        }
+    // export namespace Signature {
+    //     export function usageString(signature: Signature, localization?: Localization) {
+    //         const loc = localization?.commands[signature.command.name];
+    //         signature.arguments
+    //             .map(a => Arg.usageString(a, loc?.args ? loc.args[a.name] : undefined))
+    //             .join(" ")
+    //             + (signature.rest ? `[...${loc?.rest?.name ?? signature.rest.name}]` : "");
+    //     }
 
-        export function argumentsDescription(signature: Signature, localization: Localization) {
-            const loc = localization.commands[signature.command.name];
-            // make sure that the string is not empty
-            return signature.arguments
-                .map(a => Arg.descriptionString(a, localization.typeNames, loc?.args ? loc.args[a.name] : undefined))
-                .join("\n")
-                + (signature.rest ? `\n**[...${loc?.rest?.name ?? signature.rest.name}]** - ${loc?.rest?.description ?? signature.rest.description}` : "");
-        }
+    //     export function argumentsDescription(signature: Signature, localization: Localization) {
+    //         const loc = localization.commands[signature.command.name];
+    //         // make sure that the string is not empty
+    //         return signature.arguments
+    //             .map(a => Arg.descriptionString(a, localization.typeNames, loc?.args ? loc.args[a.name] : undefined))
+    //             .join("\n")
+    //             + (signature.rest ? `\n**[...${loc?.rest?.name ?? signature.rest.name}]** - ${loc?.rest?.description ?? signature.rest.description}` : "");
+    //     }
 
-        export function flagsDescription(signature: Signature, localization: Localization) {
-            const loc = localization.commands[signature.command.name];
-            // make sure that the string is not empty
-            return Array.from(signature.getFlags())
-                .map(f => Flag.descriptionString(f, localization.typeNames, loc?.args ? loc.args[f.name] : undefined))
-                .join('\n');
-        }
-    }
+    //     export function flagsDescription(signature: Signature, localization: Localization) {
+    //         const loc = localization.commands[signature.command.name];
+    //         // make sure that the string is not empty
+    //         return Array.from(signature.getFlags())
+    //             .map(f => Flag.descriptionString(f, localization.typeNames, loc?.args ? loc.args[f.name] : undefined))
+    //             .join('\n');
+    //     }
+    // }
 
     export namespace Arg {
-        export function usageString(argument: Arg, localization?: ParsableLocalization) {
-            if (argument.isOptional) {
+        export function usageString(argumentName: string, argument: ArgDefinition, localization?: ParsableLocalization) {
+            if (argument.optional) {
                 const val = argument.defaultValue ? ` = ${argument.defaultValue}` : '';
-                return `[${localization?.name ?? argument.name}${val}]`;
+                return `[${localization?.name ?? argumentName}${val}]`;
             } else {
-                return `<${localization?.name ?? argument.name}>`;
+                return `<${localization?.name ?? argumentName}>`;
             }
         }
 
-        export function descriptionString(argument: Arg, typeNameLocalization: TypeNameLocalization, localization?: ParsableLocalization, ) {
-            return `**${usageString(argument, localization)}** *(${typeNameLocalization[argument.type]})* - ${localization?.description ?? argument.description}`;
+        export function descriptionString(argumentName: string, argument: ArgDefinition, typeNameLocalization: TypeNameLocalization, localization?: ParsableLocalization, ) {
+            return `**${usageString(argumentName, argument, localization)}** *(${typeNameLocalization[argument.type]})* - ${localization?.description ?? argument.description}`;
         }
     }
 
     export namespace Flag {
-        export function descriptionString(flag: Flag, typeNameLocalization: TypeNameLocalization, localization?: ParsableLocalization) {
-            return `**--${flag.name}${(flag.shortcut ? ` -${flag.shortcut}` : "")}** *(${typeNameLocalization[flag.type]})* - ${localization?.description ?? flag.description}`;
+        export function descriptionString(flagName: string, flag: FlagDefinition, typeNameLocalization: TypeNameLocalization, localization?: ParsableLocalization) {
+            return `**--${flagName}${(flag.shortcut ? ` -${flag.shortcut}` : "")}** *(${typeNameLocalization[flag.type]})* - ${localization?.description ?? flag.description}`;
         }
     }
 }
