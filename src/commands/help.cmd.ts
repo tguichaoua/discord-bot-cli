0be498325d68d315adedc19fdf95faaed7a4477f
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-31 15:11:31 +0200

    ⚡️ update build-in help

--- src/commands/help.cmd.ts
+++ src/commands/help.cmd.ts
@@ -1,29 +1,26 @@
 import { Command, CommandQuery } from "../index";
 import { HelpUtility } from "../other/HelpUtility";
 import { template } from "../utils/template";
+import { makeCommand } from "../other/makeCommand";
 
-module.exports = new Command("help", {
+const cmd = makeCommand("help", {
     description: "Provide help on commands.",
-    signatures: [
-        {
-            executor: executor,
-            rest: { name: "command name", description: "The name of the command to get help." }
-        }
-    ]
+    rest: { name: "command name", description: "The name of the command to get help." }
 });
 
-async function executor({ rest, options, commandSet, message, context }: CommandQuery) {
+cmd.executor = async ({ }, { }, { rest, options, commandSet, message }) => {
     const cmdPath = rest;
 
     if (cmdPath.length === 0)
-        await message.author.send(template(options.localization.help.default, {prefix: options.prefix}));
+        await message.author.send(template(options.localization.help.default, { prefix: options.prefix }));
     else {
         const { command, args } = commandSet.resolve(cmdPath);
         if (!command || args.length != 0)
-            await message.author.send(template(options.localization.help.commandNotFound, {command: cmdPath.join(" ")}));
+            await message.author.send(template(options.localization.help.commandNotFound, { command: cmdPath.join(" ") }));
         else {
             if (options.help)
-                return await options.help({ message, options, context, command });
+                throw new Error("Not implemented");
+            //return await options.help({});
             else {
                 const embed = HelpUtility.Command.embedHelp(command, options.prefix, options.localization);
                 await message.author.send({ embed });
@@ -31,3 +28,36 @@ async function executor({ rest, options, commandSet, message, context }: Command
         }
     }
 }
+
+export default cmd;
+
+
+// module.exports = new Command("help", {
+//     description: "Provide help on commands.",
+//     signatures: [
+//         {
+//             executor: executor,
+//             rest: { name: "command name", description: "The name of the command to get help." }
+//         }
+//     ]
+// });
+
+// async function executor({ rest, options, commandSet, message, context }: CommandQuery) {
+//     const cmdPath = rest;
+
+//     if (cmdPath.length === 0)
+//         await message.author.send(template(options.localization.help.default, {prefix: options.prefix}));
+//     else {
+//         const { command, args } = commandSet.resolve(cmdPath);
+//         if (!command || args.length != 0)
+//             await message.author.send(template(options.localization.help.commandNotFound, {command: cmdPath.join(" ")}));
+//         else {
+//             if (options.help)
+//                 return await options.help({ message, options, context, command });
+//             else {
+//                 const embed = HelpUtility.Command.embedHelp(command, options.prefix, options.localization);
+//                 await message.author.send({ embed });
+//             }
+//         }
+//     }
+// }
