0da4fc3871c3cf270f3e67f5e65f613d9030b7b3
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-04 10:31:55 +0200

    ⚡️ CommandExecutor parameters readonly

--- src/models/callbacks/CommandExecutor.ts
+++ src/models/callbacks/CommandExecutor.ts
@@ -7,23 +7,23 @@ import { ParseOptions } from "../ParseOptions";
 
 export type CommandExecutor<T extends CommandDefinition = CommandDefinition> =
     (
-        args: { [name in keyof T["args"]]: (
+        args: { readonly [name in keyof T["args"]]: (
             ParsableTypeOf<NonNullable<T["args"]>[name]["type"]>
             | (
                 NonNullable<T["args"]>[name]["optional"] extends true ? undefined extends NonNullable<T["args"]>[name]["defaultValue"] ?
                 undefined : never : never
             )
         ) },
-        flags: { [name in keyof T["flags"]]: (
+        flags: { readonly [name in keyof T["flags"]]: (
             ParsableTypeOf<NonNullable<T["flags"]>[name]["type"]>
             | (undefined extends NonNullable<T["flags"]>[name]["defaultValue"] ? undefined : never)
         )
         },
         others: {
-            rest: string[];
-            message: Message & { guild: Guild | (T["guildOnly"] extends true ? never : null) };
-            guild: Guild | (T["guildOnly"] extends true ? never : null);
-            options: ParseOptions;
-            commandSet: CommandSet;
+            readonly rest: string[];
+            readonly message: Message & { readonly guild: Guild | (T["guildOnly"] extends true ? never : null) };
+            readonly guild: Guild | (T["guildOnly"] extends true ? never : null);
+            readonly options: ParseOptions;
+            readonly commandSet: CommandSet;
         }
     ) => any | Promise<any>;
