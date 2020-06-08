dc7bc9b4b49e6091466f0596f00b7fa35208ac9d
Tristan Guichaoua <baanlohdev@gmail.com>
2020-06-08 13:43:52 +0200

    🐛 fix #38

--- src/models/CommandData.ts
+++ src/models/CommandData.ts
@@ -1,15 +1,19 @@
-import { CommandDefinition } from "./definition/CommandDefinition";
+import { CommandDefinition, CommandSettings } from "./definition/CommandDefinition";
 import { CommandExecutor } from "./callbacks/CommandExecutor";
 
-export interface CommandData<T extends CommandDefinition> {
+export interface CommandData<T extends CommandDefinition, S extends CommandSettings = {}> {
     /** @internal */
     readonly def: T;
     /** @internal */
     readonly name: string;
 
     /** Called when the command is executed. */
-    executor?: CommandExecutor<T>;
+    executor?: CommandExecutor<T, Inherit<T, S>>;
 
     /** Sub-commands data. */
-    readonly subs: { readonly [name in keyof T["subs"]]: CommandData<T["subs"][name]> }
+    readonly subs: { readonly [name in keyof T["subs"]]: CommandData<T["subs"][name], Inherit<T, S>> }
 }
+
+type Inherit<T extends CommandDefinition, S extends CommandSettings> =
+    T["inherit"] extends false ? T :
+    { [k in keyof CommandSettings]-?: undefined extends T[k] ? S[k] : T[k] }
