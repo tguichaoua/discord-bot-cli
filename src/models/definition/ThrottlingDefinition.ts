fd5901df974c4bfa69eb2590d00e417d204ee9da
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-06-14 20:16:20 +0200

    Throttling enhance (#62)
    * ✨ throttling not affect admin & dev
    
    * ♻️ rename var
    
    * ✨ build-in cmd throttling
    
    * ♻️ change var name

--- src/models/definition/ThrottlingDefinition.ts
+++ src/models/definition/ThrottlingDefinition.ts
@@ -3,4 +3,6 @@ export interface ThrottlingDefinition {
     readonly count: number;
     /** Duration after which the usage count is reset. */
     readonly duration: number;
+    /** If set to true, user with administrator permission are also affected by this throttling. (default is false) */
+    readonly includeAdmins?: boolean;
 }
