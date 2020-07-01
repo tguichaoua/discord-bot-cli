c8ed63f1b31732f9d4c625a6e119e69727346860
Tristan Guichaoua <baanlohdev@gmail.com>
2020-07-01 18:50:45 +0200

    ⚡️ clientPermissions get only disctinct elements

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -2,4 +2,8 @@ export namespace ArrayUtils {
     export function isArray(o: any): o is any[] | readonly any[] {
         return Array.isArray(o);
     }
+
+    export function distinct<T>(a: T[]): T[] {
+        return [...new Set(a)];
+    }
 }
