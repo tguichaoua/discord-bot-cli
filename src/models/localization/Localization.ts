c18e5f68934bac67b3d1ce7693fdb54a6557879a
Tristan Guichaoua <baanlohdev@gmail.com>
2020-07-12 18:35:27 +0200

    ✨ display user permissions in default help

--- src/models/localization/Localization.ts
+++ src/models/localization/Localization.ts
@@ -15,6 +15,7 @@ export interface Localization {
         aliases: string;
         examples: string;
         bot_permissions: string;
+        user_permissions: string;
         default: string;
         commandNotFound: string;
         restTypeName: string;
