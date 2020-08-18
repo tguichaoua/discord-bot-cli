4531bdda6892e4badf3deda3fbcae3870ab307a3
Tristan Guichaoua <baanlohdev@gmail.com>
2020-08-18 17:10:53 +0200

    📝 fix ParseOptions doc

--- src/models/ParseOptions.ts
+++ src/models/ParseOptions.ts
@@ -1,14 +1,14 @@
 import { Localization } from "./localization/Localization";
 
 export interface ParseOptions {
-    /** The command prefix */
+    /** The command prefix. */
     prefix: string;
-    /** A list of discord user Id of dev */
+    /** A list of dev's discord ID.  */
     devIDs: readonly string[];
-    /** A localization object */
+    /** A localization object. */
     localization: Localization;
-    /** If set to true, a mention to the bot can be used instead of a prefix (default is false). */
+    /** If set to `true`, a mention to the bot can be used instead of a prefix (default is `false`). */
     allowMentionAsPrefix: boolean;
-    /** If set to true, users with dev privileges are not subject to permissions checking. (default is false) */
+    /** If set to `true`, users with dev privileges are not subject to permissions checking (default is `false`). */
     skipDevsPermissionsChecking: boolean;
 }
