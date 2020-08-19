a5d9df5831029fbd0e917eb734a42920c7b19445
Tristan Guichaoua <baanlohdev@gmail.com>
2020-08-19 14:27:35 +0200

    chore: add folder for chore scripts

---
+++ chores/release.ts
@@ -0,0 +1,9 @@
+// git checkout master
+// git pull
+// npm --no-git-tag-version version [major, minor, patch]
+// git commit -A "bump version to X.X.X"
+// git push
+// git checkout stable
+// git pull
+// git merge master
+// git push
