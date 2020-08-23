b302d1dfb420bd4bca8b306e6858c19fbff58589
Tristan Guichaoua <baanlohdev@gmail.com>
2020-08-23 11:51:39 +0200

    🐛 fix CommandSetOptions export name

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { CommandSetOptions } from "./models/CommandSetOptions";
 export { makeCommand } from "./other/makeCommand";
 export { Command } from "./models/Command";
 export { CommandSet } from "./models/CommandSet";
-export { CommandSetOptions as ParseOptions } from "./models/CommandSetOptions";
+export { CommandSetOptions } from "./models/CommandSetOptions";
 export { CommandResult } from "./models/CommandResult";
 export { Localization } from "./models/localization/Localization";
 
