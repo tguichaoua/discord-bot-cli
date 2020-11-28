6dcae29515c36c0944ea242b335010346d23ce63
Tristan Guichaoua <baanlohdev@gmail.com>
2020-11-28 15:36:21 +0100

    🚨 fix lint warning

--- chores/utils/util.ts
+++ chores/utils/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 
+//eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function editJson(path: string, action: (obj: any) => void): void {
     const o = JSON.parse(fs.readFileSync(path, "utf8"));
     action(o);
