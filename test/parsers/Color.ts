8348b30d202d37d40f5c820b1b763bae16f3c868
Tristan Guichaoua <baanlohdev@gmail.com>
2020-11-30 14:21:18 +0100

    🤡 test for custom parser

---
+++ test/parsers/Color.ts
@@ -0,0 +1,24 @@
+import { ArgProvider } from "../../src/models/parsers/ArgProvider";
+import { InvalidValueParseError } from "../../src/models/parsers/errors";
+import { Parser } from "../../src/models/parsers/Parser";
+
+export class ColorParser implements Parser<Color> {
+    parse(provider: ArgProvider): Color {
+        const r = provider.nextInteger();
+        if (r < 0 || r > 255) throw new InvalidValueParseError();
+
+        const g = provider.nextInteger();
+        if (g < 0 || g > 255) throw new InvalidValueParseError();
+
+        const b = provider.nextInteger();
+        if (b < 0 || b > 255) throw new InvalidValueParseError();
+
+        return { r, g, b };
+    }
+}
+
+interface Color {
+    r: number;
+    g: number;
+    b: number;
+}
