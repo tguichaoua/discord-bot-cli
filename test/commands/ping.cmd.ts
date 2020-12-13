ada0c2452b654eee6986217c5dc13acaee765f22
Tristan Guichaoua <baanlohdev@gmail.com>
2020-12-13 18:23:22 +0100

    🤡 test union with ping

--- test/commands/ping.cmd.ts
+++ test/commands/ping.cmd.ts
@@ -1,11 +1,14 @@
 import { makeCommand } from "../../src/index";
-import { StringParser } from "../../src/models/parsers";
+import { IntegerParser, StringParser, UnionParser } from "../../src/models/parsers";
 
 const cmd = makeCommand("ping", {
     description: "DESCRIPTION",
     args: {
         name: {
             parser: new StringParser().length(5, "name is too short, require at least 5 characters."),
+        },
+        something: {
+            parser: new UnionParser(new IntegerParser(), new StringParser()),
         },
     },
     flags: {
@@ -16,8 +19,10 @@ const cmd = makeCommand("ping", {
     },
 });
 
-cmd.executor = async ({ name }, { b }, { message }) => {
-    await message.reply(":ping_pong: Pong !\n" + `name = ${name}\nb = ${b}`);
+cmd.executor = async ({ name, something }, { b }, { message }) => {
+    await message.reply(
+        ":ping_pong: Pong !\n" + `name = ${name}\nsomething = (${typeof something}) ${something}\nb = ${b}`,
+    );
 };
 
 export default cmd;
