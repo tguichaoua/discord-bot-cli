4a1a1cbb2f0be49e9f5a5b0db85d0a5fc9a77283
Tristan Guichaoua <baanlohdev@gmail.com>
2021-02-01 13:28:18 +0100

    🏷️ export RestParser

--- src/models/parsers/index.ts
+++ src/models/parsers/index.ts
@@ -1,5 +1,6 @@
 export * from "./Parser";
 export * from "./ParsingContext";
 export * from "./buildin/primitives";
+export * from "./buildin/RestParser";
 export * from "./buildin/UnionParser";
 export * from "./errors";
