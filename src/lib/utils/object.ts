a2036cf2c5b700f5d9eac5c6cd6b369c0ed3054a
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2021-07-16 17:32:06 +0200

    🐛 export map & mapIterator utils

--- src/lib/utils/object.ts
+++ src/lib/utils/object.ts
@@ -1,10 +1,10 @@
-function* mapIterator<T, U>(iterator: Iterable<T>, fc: (o: T) => U) {
+export function* mapIterator<T, U>(iterator: Iterable<T>, fc: (o: T) => U) {
     for (const o of iterator) yield fc(o);
 }
 
-function map<K, T, U>(o: ReadonlyMap<K, T>, f: (o: T, key: K) => U): Map<K, U>;
-function map<T, U>(o: Record<string, T>, f: (o: T, key: string, index: number) => U): Record<string, U>;
-function map<T, U>(o: Record<string, T> | ReadonlyMap<unknown, T>, f: (o: T, key: string, index: number) => U) {
+export function map<K, T, U>(o: ReadonlyMap<K, T>, f: (o: T, key: K) => U): Map<K, U>;
+export function map<T, U>(o: Record<string, T>, f: (o: T, key: string, index: number) => U): Record<string, U>;
+export function map<T, U>(o: Record<string, T> | ReadonlyMap<unknown, T>, f: (o: T, key: string, index: number) => U) {
     if (o instanceof Map) {
         return new Map(mapIterator(o.entries(), ([k, v]) => [k, f(v, k, 0)] as const));
     } else {
