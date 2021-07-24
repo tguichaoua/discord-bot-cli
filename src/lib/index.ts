dead9af977281cbdf9281e3960b04164a19fef89
Tristan Guichaoua <tguichaoua.dev@gmail.com>
2021-07-24 10:55:57 +0200

    🔥 PartialCommandSetOptions

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,3 @@
-import { DeepPartial } from "./utils/DeepPartial";
-import { CommandSetOptions } from "./models/CommandSetOptions";
-
 export { makeCommand } from "./other/makeCommand";
 export { Command } from "./models/Command";
 export { CommandSet } from "./models/CommandSet";
@@ -9,8 +6,6 @@ export { CommandResult } from "./models/CommandResult";
 export { Localization } from "./models/localization/Localization";
 export * from "./models/parsers";
 export * from "./models/localization";
-
-export type PartialCommandSetOptions = DeepPartial<CommandSetOptions>;
 
 export * as HelpUtils from "./other/HelpUtils";
 export * as ListUtils from "./other/ListUtils";
